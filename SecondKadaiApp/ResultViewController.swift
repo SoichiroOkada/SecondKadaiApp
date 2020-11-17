41a765216efc19fe4df9f82edb5434121521b99a
soichiro.okada <straycat0914@gmail.com>
2020-11-17 19:48:15 +0900

    最終

--- SecondKadaiApp/ResultViewController.swift
+++ SecondKadaiApp/ResultViewController.swift
@@ -9,13 +9,17 @@
 import UIKit
 
 class ResultViewController: UIViewController {
+    // 2画面目のLabelを、StoryboardでこのViewControllerにIBOutletとして接続しておく
 
+    @IBOutlet weak var label: UILabel!
+    // 受け取るためのプロパティ（変数）を宣言しておく
+    var x:String = ""
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+                label.text = "あなたの名前は \(x) です"
     }
-    
 
     /*
     // MARK: - Navigation
